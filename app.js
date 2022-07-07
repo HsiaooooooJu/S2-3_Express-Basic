b1970da7265f1dc687b7e7f884694b6f69a1460b
HsiaooooooJu <kelllllyko@gmail.com>
2022-07-07 20:36:20 +0800

    refactor: modify express route

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
+
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+// set localhost router
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+// set port
+app.listen(3000, () => {
+  console.log('App is running on http://localhost:3000')
+})
